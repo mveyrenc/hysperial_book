5a71fb7b82b1197a73ca65625f1c5d4b22c502c3
Madeline Veyrenc <madeline@tmds.io>
2023-07-29 16:49:54 +0200

    roles in constants

--- app/models/role.rb
+++ app/models/role.rb
@@ -18,6 +18,12 @@
 #  index_roles_on_resource                                (resource_type,resource_id)
 #
 class Role < ApplicationRecord
+
+  SUPER_ADMIN = Role.where(name: 'super_admin').first_or_create(title: 'Super Admin').freeze
+  ADMIN = Role.where(name: 'admin').first_or_create(title: 'Admin').freeze
+  CONTRIBUTOR = Role.where(name: 'contributor').first_or_create(title: 'Contributor').freeze
+  READER = Role.where(name: 'reader').first_or_create(title: 'Reader').freeze
+
   # rubocop:disable Rails/HasAndBelongsToMany
   has_and_belongs_to_many :users, join_table: :users_roles
   # rubocop:enable Rails/HasAndBelongsToMany
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-super_admin_r = Role.create!(name: :super_admin, title: 'Super admin')
-Role.create!(name: :admin, title: 'Admin')
-Role.create!(name: :contributor, title: 'Contributor')
-reader_r = Role.create!(name: :reader, title: 'Reader')
+super_admin_u = User.create!(name: 'Hysperia', email: 'hysperia@gmail.com', password: 'password')
+User.create!(name: 'John Doe', email: 'john@doe.com', password: 'password')
+reader_u = User.create!(name: 'Marmion Francoeur', email: 'MarmionFrancoeur@armyspy.com', password: 'password')
 
-super_admin_u = Users.create!(name: 'Hysperia', email: 'hysperia@gmail.com', password: 'password')
-Users.create!(name: 'John Doe', email: 'john@doe.com', password: 'password')
-reader_u = Users.create!(name: 'Marmion Francoeur', email: 'MarmionFrancoeur@armyspy.com', password: 'password')
-
-UsersRole.create!(user: super_admin_u, role: super_admin_r)
-UsersRole.create!(user: reader_u, role: reader_r)
+UsersRole.create!(user: super_admin_u, role: Role::SUPER_ADMIN)
+UsersRole.create!(user: reader_u, role: Role::READER)
